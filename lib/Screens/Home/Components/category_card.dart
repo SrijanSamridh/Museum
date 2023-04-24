e1f8995c1f05c180a19526048dde4ca3104c20ec
Srijan Samridh <srijansinha3@gmail.com>
2023-04-24 21:03:56 +0530

    homescreen started

---
+++ lib/Screens/Home/Components/category_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+  final String title;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color.fromARGB(255, 252, 252, 252),
+      ),
+      
+      height: 150,
+      width: 150,
+      margin: const EdgeInsets.only(top: 40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 15, fontFamily: 'RobotoMono', color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
+import 'components/category_card.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Museum"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(40.0),
+            child: Text(
+              "Choose your category",
+              style: TextStyle(fontSize: 30, fontFamily: 'RobotoMono'),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  CategoryCard(
+                    title: "Art Gallery",
+                    icon: Icons.art_track,
+                  ),
+                  CategoryCard(
+                    title: "WWI Exhibition",
+                    icon: Icons.art_track,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  CategoryCard(
+                      title: "Exploring the space", icon: Icons.ac_unit),
+                  CategoryCard(title: "Visual Show", icon: Icons.ac_unit),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
