de1b4ce4e32ab335a8a5ceb53d93074792045d42
Srijan Samridh <srijansinha3@gmail.com>
2023-04-24 19:43:47 +0530

    inital commit

---
+++ android/app/src/main/kotlin/com/example/mmuseum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mmuseum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'Screens/Home/home_screen.dart';
+import 'Screens/LoginScreen/login_screen.dart';
+import 'Screens/SplashScreen/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const MyHomePage(),
+        '/login': (context) => const LoginScreen(),
+        '/splash': (context) => const SplashScreen(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
